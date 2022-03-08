c1c0106f5d2ee47b19aa68cd4f99e743bded5a37
sumin_desktop <csumin008255@gmail.com>
2022-03-08 22:35:03 +0900

    2022.03.08

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,14 @@
+const API_KEY = "f3490219860d57d8e5757bc9ff1c05db";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("you live it", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.name, data.weather[0].main);
+    });
 }
 
 function onGeoError() {
