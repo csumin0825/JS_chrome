3a15ac32a2b58fe0f0a41267fa12a4c2b35afb5b
sumin_desktop <csumin008255@gmail.com>
2022-03-09 14:16:25 +0900

    2022.03.09

--- js/weather.js
+++ js/weather.js
@@ -7,7 +7,10 @@ function onGeoOk(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data.name, data.weather[0].main);
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
